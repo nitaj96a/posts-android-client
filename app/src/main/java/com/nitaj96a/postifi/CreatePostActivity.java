b59de41b5cf8c27236d8cbbbcccdf3fd4f272853
n96a <nitaj1996@gmail.com>
2018-05-25 01:06:05 +0200

    CreatePostActivity GUI closes #6, closes #5

--- app/src/main/java/com/nitaj96a/postifi/CreatePostActivity.java
+++ app/src/main/java/com/nitaj96a/postifi/CreatePostActivity.java
@@ -1,13 +1,96 @@
 package com.nitaj96a.postifi;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CreatePostActivity extends AppCompatActivity {
-
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.create_post_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.create_post_drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.create_post_left_drawer);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_create_post:
+                                // Already here ...
+                                return true;
+                            case R.id.nav_all_posts:
+                                Intent intent = new Intent(getBaseContext(), PostsActivity.class);
+                                startActivity(intent);
+                                return true;
+                            case R.id.nav_settings:
+                                Intent intent_s = new Intent(getBaseContext(), SettingsActivity.class);
+                                startActivity(intent_s);
+                                return true;
+                        }
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_create_post, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_confirm:
+                Log.i("confirm", "clicked confirm");
+                return true;
+
+            case R.id.action_cancel:
+                Log.i("cancel", "clicked cancel");
+                return true;
+
+            // no "home" id...
+            case R.id.homeAsUp:
+                Log.i("home", "clicked home");
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+            default:
+                Log.i("default", "clicked default");
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                Log.i("default", "after drawer");
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
