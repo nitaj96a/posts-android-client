e87aa6a108df560b95d0970e3944a00e2c0466b5
n96a <nitaj1996@gmail.com>
2018-05-27 18:05:57 +0200

    added ServiceUtils class \n instantiated a Retrofit object...

---
+++ app/src/main/java/com/nitaj96a/postifi/Service/ServiceUtils.java
@@ -0,0 +1,35 @@
+package com.nitaj96a.postifi.Service;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by n on 5/27/2018.
+ */
+
+public class ServiceUtils {
+
+    static final String BASE_URL="http:127.0.0.1:8080/api/";
+
+    public static OkHttpClient createClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .addInterceptor(interceptor).build();
+        return client;
+    }
+
+    public static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(createClient())
+            .build();
+}
